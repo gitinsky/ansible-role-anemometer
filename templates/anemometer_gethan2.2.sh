00cc8c7b3e7fc921397efb42c171d5e3fa06436e
Alex Chistyakov <alexclear@gmail.com>
2015-08-31 22:15:56 +0300

    Empty log after processing

--- templates/anemometer_gethan2.2.sh
+++ templates/anemometer_gethan2.2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 pt-query-digest --user=anemometer --password={{ anemometer_mysql_password }} --review h={{ anemometer_mysql_ip }},D=slow_query_log,t=global_query_review --history h={{ anemometer_mysql_ip }},D=slow_query_log,t=global_query_review_history --no-report --limit=0% --filter=" \$event->{Bytes} = length(\$event->{arg}) and \$event->{hostname}=\"localhost\"" {{ anemometer_slow_log_path }}
+> {{ anemometer_slow_log_path }}
